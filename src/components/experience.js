d1af5b29b45f6f3f54e763062ad5cfb215323058
Jefree Sujit <e.jefree110@gmail.com>
2018-04-12 20:48:42 +0530

    updated experience section

--- src/components/experience.js
+++ src/components/experience.js
@@ -11,10 +11,11 @@ class Experience extends React.Component {
         <div className="job-section">
           <div className="job-title">Front End Engineer</div>
           <div className="company">Optum Analytics</div>
-          <div className="exp">(Mar 2018 - Present)</div>
+          <div className="exp">(Apr 2018 - Present)</div>
           <div className="role-rescription">
-            <p className="desc-text">• Building a Member Self-Service Application, an Enterprise Level SPA with Material Inspired designs for user-friendly experience. ES6, React.js, Redux, SASS, Webpack.</p>
-            <p className="desc-text">• Building the Middle-Tier Server for WebApp, serving data requests from WebApp and authentication for requests from external sources. NPM, Node.js, Express.js.</p>
+            <p className="desc-text">• Firm rooting the "Console" application with solid technology stacks, making it more modular and as a platform application to support for any framework to be built in.</p>
+            <p className="desc-text">• Enhancing the middle tier server to handle cross-application requests as a platform, with authentication and authorization and serving custom user role based features</p>
+            <p className="desc-text">• Working on AWS getting application to cloud, handling traffic with load balancing and autoscaling, improving performance and reducing cost.</p>
           </div>
         </div>
         <div className="job-section">
@@ -22,7 +23,7 @@ class Experience extends React.Component {
           <div className="company">The Advisory Board Company</div>
           <div className="exp">(Jun 2016 - Mar 2018)</div>
           <div className="role-rescription">
-            <p className="desc-text">• Building a Member Self-Service Application, an Enterprise Level SPA with Material Inspired designs for user-friendly experience. ES6, React.js, Redux, SASS, Webpack.</p>
+            <p className="desc-text">• Building a Member Self-Service Application "Console", an Enterprise Level SPA with Material Inspired designs for user-friendly experience. ES6, React.js, Redux, SASS, Webpack.</p>
             <p className="desc-text">• Building the Middle-Tier Server for WebApp, serving data requests from WebApp and authentication for requests from external sources. NPM, Node.js, Express.js.</p>
           </div>
         </div>
